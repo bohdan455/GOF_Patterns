7575b568867fb47a86b90e05612ffa98f780c9f7
Bohdan <bohdan_voroniak@epam.com>
2025-05-03 17:52:23 +0300

    Add builder pattern

---
+++ Creational/Builder/Builder.md
@@ -0,0 +1,6 @@
+﻿The Builder pattern is great for creating an object step by step by calling its methods, instead of constructing it using a large constructor.
+
+We create a separate Builder class that is responsible for assembling the object.
+Different builders can be used if we need to create different types of objects.
+
+Additionally, we can have a separate class called something like a Director, which is responsible for creating the object using the Builder class and calling the methods in the correct order.
---
+++ Creational/Builder/Example.cs
@@ -0,0 +1,67 @@
+﻿class Trial
+{
+    public DateTime Start { get; set; }
+    public DateTime End { get; set; }
+    public string Name { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
+
+class TrialBuilder
+{
+    private Trial _trial = new Trial();
+
+    public TrialBuilder Reset()
+    {
+        _trial = new Trial();
+        return this;
+    }
+
+    public TrialBuilder AddDateRange(DateTime start, DateTime end)
+    {
+        _trial.Start = start;
+        _trial.End = end;
+        return this;
+    }
+
+    public TrialBuilder AddName(string name)
+    {
+        _trial.Name = name;
+        return this;
+    }
+
+    public TrialBuilder AddTitle(string title)
+    {
+        _trial.Title = title;
+        return this;
+    }
+
+    public TrialBuilder AddDescription(string description)
+    {
+        _trial.Description = description;
+        return this;
+    }
+
+    public Trial Build() => _trial;
+}
+
+class TrialDirector
+{
+    private readonly TrialBuilder _builder;
+    
+    // Can use interface of builder for bettern testablity
+    public TrialDirector(TrialBuilder builder)
+    {
+        _builder = builder;
+    }
+    
+    public Trial BuildOneYearTrial()
+    {
+        return _builder
+            .Reset()
+            .AddDateRange(DateTime.UtcNow, DateTime.UtcNow.AddMonths(12))
+            .AddTitle("1-Year Trial")
+            .AddDescription("A full one-year trial.")
+            .Build();
+    }
+}
