49a4772bc4a6450d6200ed42adb8db6da46b3d5e
Bohdan <bohdan_voroniak@epam.com>
2025-05-03 18:08:09 +0300

    Add prototype pattern

---
+++ Creational/Prototype/Description.md
@@ -0,0 +1,3 @@
+﻿The Prototype pattern is useful when creating new objects is expensive or complex, and we want to create a duplicate (clone) of an existing object instead.
+It allows us to make copies of objects at runtime without knowing their exact class, using a cloning interface (like ICloneable in C#).
+This is especially useful when object creation involves heavy configuration or external resources.
---
+++ Creational/Prototype/Example.cs
@@ -0,0 +1,27 @@
+﻿namespace DefaultNamespace;
+
+public interface IPrototype<T>
+{
+    T DeepCopy();
+    T ShallowCopy();
+}
+
+public class ObjectToCopy : IPrototype<ObjectToCopy>
+{
+    public string Name { get; set; }
+    public int[] Values { get; set; }
+
+    public ObjectToCopy DeepCopy()
+    {
+        return new ObjectToCopy
+        {
+            Name = Name,
+            Values = (int[])Values.Clone()
+        };
+    }
+
+    public ObjectToCopy ShallowCopy()
+    {
+        return (ObjectToCopy)this.MemberwiseClone();
+    }
+}
