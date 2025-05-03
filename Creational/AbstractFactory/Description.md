357f12c28b93032094561fbfcca1585209c656e4
Bohdan <bohdan_voroniak@epam.com>
2025-05-03 17:29:17 +0300

    add abstract factory pattern

---
+++ Creational/AbstractFactory/Description.md
@@ -0,0 +1,12 @@
+﻿When we need to create similar objects with a common parent class but with different logic inside them, we can use the Abstract Factory pattern.
+
+An Abstract Factory class is responsible only for creating objects and doesn't contain any logic itself.
+It also implements a factory interface, so we can have multiple factories where each returns a similar base-type object but with different implementations.
+
+When we need to use this factory, we can simply use the factory interface, and the user of our logic would decide which factory to provide.
+
+Typical use case is when we need to do some work on one group of object but don't want to depend on their realization
+
+For example, we create a payment processor, and every processor has its own Transaction, Account, and L2L3Data.
+
+The difference between the Factory Method and Abstract Factory is that the Factory Method creates a single type of object, while the Abstract Factory is responsible for creating a family of related objects that are meant to be used together.
---
+++ Creational/AbstractFactory/Example.cs
@@ -0,0 +1,87 @@
+﻿namespace DefaultNamespace;
+
+abstract class Transaction
+{
+    // Some logic
+}
+
+class PayPalTransaction : Transaction
+{
+    // Some logic
+}
+
+class CreditCardTransaction : Transaction
+{
+    // Some logic
+}
+abstract class Account
+{
+    // Some logic
+}
+
+class CreditCardAccount : Account
+{
+    // Some logic
+}
+
+class PayPalAccount : Account
+{
+    // Some logic
+}
+
+abstract class L2L3Data
+{
+    // Some logic
+}
+
+class CreditCardL2L3Data : L2L3Data
+{
+    // Some logic
+}
+
+class PayPalL2L3Data : L2L3Data
+{
+    // Some logic
+}
+
+interface IPaymentFactory
+{
+    Transaction GetTransaction();
+    Account GetAccount();
+    L2L3Data GetL2L3Data();
+}
+
+class PayPalFactory : IPaymentFactory
+{
+    public Transaction GetTransaction() => new PayPalTransaction();
+    public Account GetAccount() => new PayPalAccount();
+    public L2L3Data GetL2L3Data() => new PayPalL2L3Data();
+}
+
+class CreditCardFactory : IPaymentFactory
+{
+    public Transaction GetTransaction() => new CreditCardTransaction();
+    public Account GetAccount() => new CreditCardAccount();
+    public L2L3Data GetL2L3Data() => new CreditCardL2L3Data();
+}
+
+class PaymentSystemProccessor
+{
+    private readonly IPaymentFactory _paymentFactory;
+    
+    public PaymentSystemProccessor(IPaymentFactory paymentFactory)
+    {
+        _paymentFactory = paymentFactory;
+    }
+
+    public void Process()
+    {
+        // Some logic
+        var transaction = _paymentFactory.GetTransaction();
+        // Some logic
+        var account = _paymentFactory.GetAccount();
+        // Some logic
+        var l2l3Data = _paymentFactory.GetL2L3Data();
+        // Some logic
+    }
+}
